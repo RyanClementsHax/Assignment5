61973cd8f0ac2a219e811bf52493b440086f5501
RyanClementsHax <ryanclementsuf@gmail.com>
2017-12-13 14:32:33 -0500

    Added additional functionality

--- db-ui/src/app/app.component.ts
+++ db-ui/src/app/app.component.ts
@@ -7,21 +7,65 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  inputState: any;
-  states = ["Florida", "Georgia"];
+  inputSighting: any = { person: '', location: '', sighted: '' };
+  selectedFlower: any = { comname: '', species: '', genus: '', sightings: [] };
+  flowers = [
+    { 
+      comname: "Lily",
+      genus: "hello",
+      species: "world",
+      sightings: [
+        {
+          person: "mary",
+          location: "garden",
+          sighted: "12/06/1996"
+        },
+        {
+          person: "joseph",
+          location: "eden",
+          sighted: "12/06/1998"
+        }
+      ]
+    }, 
+    {
+      comname: "Rose",
+      genus: "what",
+      species: "jesus",
+      sightings: [
+        {
+          person: "adam",
+          location: "nazarath",
+          sighted: "2/09/1986"
+        },
+        {
+          person: "eve",
+          location: "eve",
+          sighted: "11/07/1998"
+        }
+      ]
+    }
+  ];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() { 
-    this.http.get<Array<string>>('/list').subscribe(data => {
-      this.states = data;
+    this.http.get<any>('/list').subscribe(data => {
+      this.flowers = data;
     });
   }
 
-  addState() {
-    this.states.push(this.inputState);
-    this.inputState = '';
+  addSighting() {
+    this.selectedFlower.sightings.push(this.inputSighting);
+    this.inputSighting = { person: '', location: '', sighted: '' };
 
-    this.http.post('/add', this.inputState);
+    this.http.post('/update', this.selectedFlower);
+  }
+
+  updateFlower() {
+    this.http.post('/update', this.selectedFlower);
+  }
+
+  selectFlower(flower: any) {
+    this.selectedFlower = flower;
   }
 }
